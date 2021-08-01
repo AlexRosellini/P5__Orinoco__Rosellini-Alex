3c3bef40f1cd43975dbe4ca4a8c40dc35d8d16a9
Alex.Rosellini <haloodstboss@live.fr>
2021-08-01 14:53:00 +0200

    ajout de 'Ajouter au panier'

--- JS/produit.js
+++ JS/produit.js
@@ -16,23 +16,22 @@ fetch(`http://localhost:3000/api/teddies/${id}`)
     let htmlProduit = `<div class="card">
     <div class="card__name"><h3>${response.name}</h3></div>
     <div class="card__img"><img src="${response.imageUrl}"></div>
-    <div class="card__description"><p>${response.description}</p></div>
+    <div class="card__description"><p>${response.description}</p></div>  
     <div class = "card__bottom">
     <div class="card__price"><p>Price : ${response.price/100} €</p></div>
     <label class="card__label"><h3>Personalisez votre ours!</h3></label>
     <select class="card__colors"></select>
-    <div class="card__button><p>Ajouter au panier</p></div>
+    <div class="card__button"><p>Ajouter au panier</p></div>
     </div>
     </div>`
-
+    
+    document.querySelector(".card").innerHTML = htmlProduit
 
     /*
     notre but est ensuite d'ajouter chacune des options à nos produits, soit en notre cas, des couleurs. 
     pour ce faire, on utilise une boucle for avec en paramètre 'j' qui va se positioner sur chaque couleur. Ensuite, on créer les éléements
     via create element, classname et textcontent ou nous ajoutons nos options.
     */
-
-    document.querySelector(".card").innerHTML = htmlProduit
 
     let choice = document.querySelector('.card__colors')
     for (j = 0; j < response.colors.length; j++) {
