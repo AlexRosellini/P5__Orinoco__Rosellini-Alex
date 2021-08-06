4a79769c5f4257000ae4c46ad29d4f3ca48ef26f
Alex.Rosellini <haloodstboss@live.fr>
2021-08-06 15:35:26 +0200

    ajout regex et formulaire

--- JS/panier.js
+++ JS/panier.js
@@ -1,3 +1,4 @@
+
 let userPanier = JSON.parse(localStorage.getItem("produit"));
 console.log(userPanier);
 
@@ -31,7 +32,31 @@ for (let l = 0; l < userPanier.length; l++) {
     console.log(iteratePrice)
 }
 
-
+let form = `<h2>Merci de remplir le formulaire ci-dessous pour completer votre commande</h2>
+<div class="contact">
+    <div class="contact__info">
+        <label for="firstName">Prénom</label>
+        <input type="text" name="firstName" class="firstName" placeholder="Nero" required>
+    </div>
+    <div class="contact__info">
+        <label for="lastName">Nom de famille</label>
+        <input type="text" name="lastName" class="lastName" placeholder="Claudius" required>
+    </div>
+    <div class="contact__info">
+        <label for="adress">Adresse</label>
+        <input type="text" name="address" class="address" placeholder="Via della Domus Aurea, 1, 00184" required>
+    </div>
+    <div class="contact__info">
+        <label for="city">ville</label>
+        <input type="text" name="city" class="city" placeholder="Rome" required>
+    </div>
+    <div class="contact__info">
+        <label for="email">Adresse Email</label>
+        <input type="text" name="email" class="email" placeholder="UmuEmpresspadoru@roma.rome" required>
+    </div>
+    <button class="sendForm">Valider la commande</button>
+</div>
+`
 
 const reducer = (accumulator, currentValue) => accumulator + currentValue; {
      calculPrice =  iteratePrice.reduce(reducer);
@@ -40,4 +65,58 @@ const reducer = (accumulator, currentValue) => accumulator + currentValue; {
 }
 
 let finalPrice = `<div call = panier__finalPrice><p> ${calculPrice} €</p></div>`
-document.querySelector('.panier').innerHTML = fullCart + finalPrice ;
+document.querySelector('.panier').innerHTML = fullCart + finalPrice + form ;
+
+let sendform = document.querySelector('.sendForm')
+sendform.addEventListener('click', (event) => {
+
+let contact = {
+    firstName: document.querySelector(".firstName").value,
+    lastName: document.querySelector(".lastName").value,
+    address: document.querySelector(".address").value,
+    city: document.querySelector(".city").value,
+    email: document.querySelector(".email").value,
+}
+
+
+const formvalueP = contact.firstName;
+const formvalueL = contact.lastName;
+const formvalueA = contact.address;
+const formvalueC = contact.city;
+const formvalueE = contact.email;
+
+function namecheck(){
+ 
+    if (/^[A-Za-z]{2,24}$/.test(formvalueP) && (/^[A-Za-z]{2,24}$/.test(formvalueL))) { 
+            return true  
+    } 
+}
+
+function citycheck(){
+    if (/^[A-Za-z]{2,24}$/.test(formvalueC)) {
+        return true
+    }
+}
+
+function emailcheck(){
+ 
+    if (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(formvalueE)) { 
+            return true  
+    } 
+}
+
+function addresscheck(){
+ 
+    if (/^(([a-zA-ZÀ-ÿ0-9]+[\s\-]{1}[a-zA-ZÀ-ÿ0-9]+)){1,10}$/.test(formvalueA)) { 
+            return true  
+    } else { 
+        console.log("no")
+    }
+}
+
+if (namecheck() == true && emailcheck() == true && addresscheck() == true && citycheck() == true) {
+    console.log(contact)
+} else {
+    console.log('formulaire invalide')    
+}
+})
